cdf4e00bb9cbac340b2466cfacef00ffd54b4129
Thinh Nguyen <ducthinh993@gmail.com>
2026-04-19 12:22:41 +0700

    feat: delete the deprecated vscode-extension submodules

--- vscode-extension/out/providers/hover.d.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import { Commit0Api } from '../api.js';
-export declare class Commit0HoverProvider implements vscode.HoverProvider {
-    private api;
-    private cachedSlug;
-    constructor(api: Commit0Api);
-    private getRepoSlug;
-    provideHover(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.Hover | null>;
-}
